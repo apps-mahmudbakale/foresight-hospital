18873349149db9589b618684050001d9866393af
Mahmud Bakale <5157015-apps-mahmudbakale@users.noreply.gitlab.com>
2021-07-03 12:24:13 +0100

    WIP

--- about.php
+++ about.php
@@ -28,6 +28,7 @@
 
     <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
     <link href="assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
   </head>
 
--- index.php
+++ index.php
@@ -129,24 +129,25 @@
 					</div>
 					<div class="form-row">
 						<div class="col-md-4 form-group">
-							<input type="datetime" name="date" class="form-control datepicker" id="date" placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
+							<input type="date" name="date" class="form-control datepicker" id="date" placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars">
 							<div class="validate"></div>
 						</div>
 						<div class="col-md-4 form-group">
 							<select name="department" id="department" class="form-control">
 								<option value="">Select Department</option>
-								<option value="Department 1">Department 1</option>
-								<option value="Department 2">Department 2</option>
-								<option value="Department 3">Department 3</option>
+								<option value="Clinic">Clinic</option>
+								<option value="Surgery">Surgery</option>
+								<option value="Clinical Medicine">Clinical Medicine</option>
+								<option value="Diagnostic Services & Laboratory">Diagnostic Services & Laboratory</option>
 							</select>
 							<div class="validate"></div>
 						</div>
 						<div class="col-md-4 form-group">
 							<select name="doctor" id="doctor" class="form-control">
 								<option value="">Select Doctor</option>
-								<option value="Doctor 1">Doctor 1</option>
-								<option value="Doctor 2">Doctor 2</option>
-								<option value="Doctor 3">Doctor 3</option>
+								<option value="Dr. Shamsuddeen">Dr. Shamsuddeen Aliyu</option>
+								<option value="Dr. Jalaluddeen">Dr. Jalaluddeen M. Bashir</option>
+								<option value="Dr. Chuks Okolouta">Dr. Chuks Okolouta</option>
 							</select>
 							<div class="validate"></div>
 						</div>
