78ccc6f4bdf2f76ebfd978636cfde3ec865155b6
Mahmud Bakale <apps-mahmudbakale@gmail.com>
2021-07-06 10:58:58 -0500

    WIP

--- index.php
+++ index.php
@@ -266,49 +266,7 @@
 		<a href="https://api.whatsapp.com/send?phone=+2348038969767&text=Hello%21%20Mahmud Bakale" class="float" target="_blank">
 			<i class="fa fa-whatsapp my-float"></i>
 		</a>
-		<?php 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-		use PHPMailer\PHPMailer\PHPMailer;
-		use PHPMailer\PHPMailer\SMTP;
-		use PHPMailer\PHPMailer\Exception;
-
-//Load Composer's autoloader
-		require 'vendor/autoload.php';
-
-//Create an instance; passing `true` enables exceptions
-		$mail = new PHPMailer(true);
-
-		try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'bakale.mahmud@gmail.com';                     //SMTP username
-    $mail->Password   = 'bakale123';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('contact@foresightclinics.com', 'Mailer');
-    $mail->addAddress('bakale2013@gmail.com', 'Joe User');     //Add a recipient
-    $mail->addReplyTo('contact@foresightclinics.com', 'Information');
-
-
-    //Content
-    $mail->isHTML(true);         //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
-
-?>
+		
 </main><!-- End #main -->
 <?php include 'inc/footer.inc.php'; ?>
 <div id="preloader"></div>
