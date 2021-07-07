8333690ce21f4ae1b0d98e8b7858813e5a16fe17
Mahmud Bakale <apps-mahmudbakale@gmail.com>
2021-07-07 01:57:44 -0500

    WIP

--- Admin/index.php
+++ Admin/index.php
@@ -32,10 +32,16 @@ include 'connection.php';
     <div class="card">
       <div class="card-body login-card-body">
         <p class="login-box-msg">Sign in to start your session</p>
+<?php 
+if (isset($_POST['login'])) {
+  $username = $_POST['username'];
+  $password = md5($_POST['password']);
+}
 
+ ?>
         <form action="" method="post">
           <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email">
+            <input type="text" name="username" class="form-control" placeholder="Email">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-envelope"></span>
@@ -43,7 +49,7 @@ include 'connection.php';
             </div>
           </div>
           <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password">
+            <input type="password" name="password" class="form-control" placeholder="Password">
             <div class="input-group-append">
               <div class="input-group-text">
                 <span class="fas fa-lock"></span>
@@ -61,7 +67,7 @@ include 'connection.php';
             </div>
             <!-- /.col -->
             <div class="col-4">
-              <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+              <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
             </div>
             <!-- /.col -->
           </div>
